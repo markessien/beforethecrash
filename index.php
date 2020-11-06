d3d6bf78940a129ece48810c393b6d691db32187
tokoytg <tboynonilekan@gmail.com>
2020-11-06 11:39:13 +0000

    looping through server disks

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
 <?php
 
 include('libs/helpers.php');
-
+date_default_timezone_set("Africa/Lagos");
 $disk_info = shell_exec('df -h');
 $formatted = parseDfResponse($disk_info);
 $server_disk =$formatted['filesystems']['/dev/nvme0n1p6'];
-$free_space = parse_free_space($server_disk['available']);
-print_r($free_space);
+$all_server_disks =$formatted['filesystems'];
+foreach($all_server_disks as $server_disk){
+    $free_space = parse_free_space($server_disk['available']);
+    if($free_space <= 1024){
+        echo "warning email";
+    }
+    echo $free_space."<br/>";
+}
+$formatted['filesystems']['created_at'] =  date('d M Y h:i:sa');
+file_put_contents("datastore/disk_info.json", json_encode($formatted['filesystems']));
 
 
 
-
-
+print_r(getenv());
 
 
 
