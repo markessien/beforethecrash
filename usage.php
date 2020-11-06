539223d4dbd28e6d13275b8f41c930589f75d899
emmaakachukwu <emmanuelakachukwu1@gmail.com>
2020-11-06 14:23:36 +0100

    little change

--- usage.php
+++ usage.php
@@ -9,11 +9,21 @@ function get_current_users() {
     $to = date('d-m-y', strtotime('+ 7 days'));
     $path = "datastore/usage ~ ${from} _ ${to}.php";
 
-    foreach ($split_users as $user) {
-        if ( !empty(trim($user)) ) {
-            file_put_contents($path, "\n" . trim($user), FILE_APPEND);
-        }
+    $get_files = array_slice(scandir("datastore/"), 2);
+    
+    if ( count($get_files) ) {
+        $last_file = $get_files[count($get_files) - 1];
+        
+        print_r($last_file);
     }
+
+    // print_r($get_files);
+
+    // foreach ($split_users as $user) {
+    //     if ( !empty(trim($user)) ) {
+    //         file_put_contents($path, "\n" . trim($user), FILE_APPEND);
+    //     }
+    // }
 }
 
 get_current_users();
