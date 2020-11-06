f599996704c2fd3b94de27e17d45c15b1b8ceec1
emmaakachukwu <emmanuelakachukwu1@gmail.com>
2020-11-06 15:08:52 +0100

    checking for dates weekly

--- usage.php
+++ usage.php
@@ -1,29 +1,37 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
 /* RETRIEVE CURRENTLY LOGGED IN USERS */
 function get_current_users() {
     $logged_in_users = shell_exec('who');
     $split_users = explode("\n", $logged_in_users);
-    
-    $from = date('d-m-y');
-    $to = date('d-m-y', strtotime('+ 7 days'));
-    $path = "datastore/usage ~ ${from} _ ${to}.php";
 
-    $get_files = array_slice(scandir("datastore/"), 2);
+    $get_files = array_slice(scandir("datastore/usage_files/"), 2);
     
     if ( count($get_files) ) {
         $last_file = $get_files[count($get_files) - 1];
-        
-        print_r($last_file);
+        $date_string = trim(explode('_', explode('.', $last_file)[0])[1]);
+        if ( strtotime($date_string) < time() ) {
+            $from = date('d-m-y');
+            $to = date('d-m-y', strtotime('+ 7 days'));
+            $path = "datastore/usage_files/usage ~ ${from} _ ${to}.php";
+        } else {
+            $from = date('d-m-y');
+            $to = date('d-m-y', strtotime('+ 7 days'));
+            $path = "datastore/usage_files/${last_file}";
+        }
+    } else {
+        $from = date('d-m-y');
+        $to = date('d-m-y', strtotime('+ 7 days'));
+        $path = "datastore/usage_files/usage ~ ${from} _ ${to}.php";
     }
 
-    // print_r($get_files);
-
-    // foreach ($split_users as $user) {
-    //     if ( !empty(trim($user)) ) {
-    //         file_put_contents($path, "\n" . trim($user), FILE_APPEND);
-    //     }
-    // }
+    foreach ($split_users as $user) {
+        if ( !empty(trim($user)) ) {
+            file_put_contents($path, "\n" . trim($user), FILE_APPEND);
+        }
+    }
 }
 
 get_current_users();
