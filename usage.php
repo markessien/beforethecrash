8fc6ec84fd0592665a2b68abeffa66dcf8cf23ab
emmaakachukwu <emmanuelakachukwu1@gmail.com>
2020-11-09 06:35:30 +0100

    fixes done

--- usage.php
+++ usage.php
@@ -6,25 +6,22 @@ error_reporting(E_ALL);
 function get_current_users() {
     $logged_in_users = shell_exec('who');
     $split_users = explode("\n", $logged_in_users);
-
     $get_files = array_slice(scandir("datastore/usage_files/"), 2);
     
     if ( count($get_files) ) {
         $last_file = trim($get_files[count($get_files) - 1]);
         $date_string = trim(explode('_', explode('.', $last_file)[0])[1]);
         if ( strtotime($date_string) < time() ) {
-            $from = date('d-m-y');
-            $to = date('d-m-y', strtotime('+ 7 days'));
-            $path = "datastore/usage_files/usage ~ ${from} _ ${to}.php";
+            $from = date('d-m-Y');
+            $to = date('d-m-Y', strtotime('+ 7 days'));
+            $path = "datastore/usage_files/usage ~ ${from} _ ${to}.txt";
         } else {
-            $from = date('d-m-y');
-            $to = date('d-m-y', strtotime('+ 7 days'));
             $path = "datastore/usage_files/${last_file}";
         }
     } else {
-        $from = date('d-m-y');
-        $to = date('d-m-y', strtotime('+ 7 days'));
-        $path = "datastore/usage_files/usage ~ ${from} _ ${to}.php";
+        $from = date('d-m-Y');
+        $to = date('d-m-Y', strtotime('+ 7 days'));
+        $path = "datastore/usage_files/usage ~ ${from} _ ${to}.txt";
     }
 
     foreach ($split_users as $user) {
